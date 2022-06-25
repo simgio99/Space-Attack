116013cdf48221beb51566261cc9d037229259d9
pilots2013 <56370765+pilots2013@users.noreply.github.com>
2022-06-26 01:07:07 +0200

    fix

--- crypt.py
+++ crypt.py
@@ -1,8 +1,10 @@
 import string
-#JaAbDk1QlerxhNo8pLqS2Q==C52TB!9DC6! T6BTC!TDB2Tx T6B9TC!Tz!00D 6zxC2TC29202CAHTC!TC52T0x6
-#TBCxC6! WTq52T 2GCT82HT6BTJSnij8GNMQUx06N++TLehaxw==
+import base64
+
+def base64decode(str):
+    return base64.decode(str)
+#Function that computes the sorted dictionary of the most frequent characters in a string
 def countOccurrences(strn):
-    print("Prova")
     charDict = {}
     for ch in string.printable:
         charDict[ch] = 0
@@ -10,8 +12,43 @@ def countOccurrences(strn):
         charDict[ch] += 1
     return sorted(charDict, key=charDict.get, reverse=True)
 
-decipherDict = {
-    'T' : ' ',
+
+#Function that returns the string obtained as translation of the argument string with the specified decipher dictionary
+def mapString(strn, decipherDict):
+    finalString = ""
+    for ch in strn:
+        if(ch in decipherDict):
+            finalString += decipherDict[ch]
+        else:
+            finalString += ch
+    return finalString
+
+#Function that returns the count of the unique occurences of characters in the given string
+def countUnique(strn):
+    uniques = set(strn)
+    print("UNIQUE OCCURENCES: " + str(len(uniques)))
+
+#Function that implements a decipher method based on modulo arithmetic with the given string    
+def modDecipher(strn, modn):
+    finalString = ""
+    for ch in strn: 
+        finalString += (chr((65 + (ord(ch) + modn) % 26)))
+    return finalString
+
+#Function that implements the skip-n decryption algorithm with the given string and skip-size
+def skipDecipher(strn, skipSize):
+    finalString = ""
+    counter = 0
+    while len(finalString) < len(strn):
+        finalString += strn[counter]
+        counter = (counter + skipSize + 1) % len(strn)
+    return finalString
+
+
+
+def main():
+    decipherDict = {
+    'T' : ' ', #ok
     '2' : 'e', #ok
     'C' : 't', #ok
     '6' : 'i', #ok
@@ -30,46 +67,13 @@ decipherDict = {
     'q' : 't', #ok 
     'G' : 'x', #ok
     '8' : 'k', #ok
-    'J' : 'J', #a or A or 0 or 1
-    'S' : 'S', #z or 
+    'J' : '1', #a or A or 0 or 1
+    'S' : '0', #z or 
 }
-
-def mapString(strn):
-    finalString = ""
-    for ch in strn:
-        if(ch in decipherDict):
-            finalString += decipherDict[ch]
-        else:
-            finalString += ch
-    return finalString
-        
-def countUnique(strn):
-    uniques = set(strn)
-    print("UNIQUE OCCURENCES: " + str(len(uniques)))
-
     
-def decipher(strn, modn):
-    finalString = ""
-    for ch in strn: 
-        finalString += (chr((65 + (ord(ch) + modn) % 26)))
-    return finalString
-
-def skipDecipher(strn, skipSize):
-    finalString = ""
-    counter = 0
-    while len(finalString) < len(strn):
-        finalString += strn[counter]
-        counter = (counter + skipSize + 1) % len(strn)
-    return finalString
-
-
-
-def main():
-    print("bruh")
     strn = "C52TB!9DC6! T6BTC!TDB2Tx T6B9TC!Tz!00D 6zxC2TC29202CAHTC!TC52T0x6 TBCxC6! WTq52T 2GCT82HT6BTJS"
     print(countOccurrences("C52TB!9DC6! T6BTC!TDB2Tx T6B9TC!Tz!00D 6zxC2TC29202CAHTC!TC52T0x6 TBCxC6! WTq52T 2GCT82HT6BTJS"))
-    #print(decipher(strn, 15))
-    print(mapString(strn))
+    print(mapString(strn, decipherDict))
     countUnique(strn)
     
     strn2 = "Ttoleenstidhyw sst. s1e nt  a k 2 ilhotiTep3psoefhnhya4r a  a e s5itdiNts As6oo ma hlEw7r aapciaSo8idlglops r"
