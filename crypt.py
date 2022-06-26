97bc8f27f9e47fa5f908c4f4261a13d7a7fb1c72
Michele Aversana <56485009+Breiart@users.noreply.github.com>
2022-06-26 15:41:47 +0200

    Update crypt.py

--- crypt.py
+++ crypt.py
@@ -1,9 +1,11 @@
 import string
 import base64
 
+
 def base64decode(str):
     return base64.decode(str)
-#Function that computes the sorted dictionary of the most frequent characters in a string
+
+# Function that computes the sorted dictionary of the most frequent characters in a string. We used it to execute the frequency analysis
 def countOccurrences(strn):
     charDict = {}
     for ch in string.printable:
@@ -13,7 +15,8 @@ def countOccurrences(strn):
     return sorted(charDict, key=charDict.get, reverse=True)
 
 
-#Function that returns the string obtained as translation of the argument string with the specified decipher dictionary
+# Function that returns the string obtained as translation of the argument string with the specified decipher dictionary
+# We created it while working for the level 3 of the challenge, in order to simplify its resolution
 def mapString(strn, decipherDict):
     finalString = ""
     for ch in strn:
@@ -28,7 +31,8 @@ def countUnique(strn):
     uniques = set(strn)
     print("UNIQUE OCCURENCES: " + str(len(uniques)))
 
-#Function that implements a decipher method based on modulo arithmetic with the given string    
+#Function that implements a decipher method based on modulo arithmetic with the given string
+# This was our first attempt for level 4
 def modDecipher(strn, modn):
     finalString = ""
     for ch in strn: 
@@ -36,6 +40,7 @@ def modDecipher(strn, modn):
     return finalString
 
 #Function that implements the skip-n decryption algorithm with the given string and skip-size
+# This function allowed us to decrypt level 4
 def skipDecipher(strn, skipSize):
     finalString = ""
     counter = 0
@@ -71,11 +76,13 @@ def main():
     'S' : '0', #z or 
 }
     
+    # Test script we used to decrypt level 3
     strn = "C52TB!9DC6! T6BTC!TDB2Tx T6B9TC!Tz!00D 6zxC2TC29202CAHTC!TC52T0x6 TBCxC6! WTq52T 2GCT82HT6BTJS"
     print(countOccurrences("C52TB!9DC6! T6BTC!TDB2Tx T6B9TC!Tz!00D 6zxC2TC29202CAHTC!TC52T0x6 TBCxC6! WTq52T 2GCT82HT6BTJS"))
     print(mapString(strn, decipherDict))
     countUnique(strn)
     
+    # Test script we used to decrypt level 4
     strn2 = "Ttoleenstidhyw sst. s1e nt  a k 2 ilhotiTep3psoefhnhya4r a  a e s5itdiNts As6oo ma hlEw7r aapciaSo8idlglops r"
     print(skipDecipher(strn2, 10))
     
